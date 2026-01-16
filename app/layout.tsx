7a5715d0f0c3757976074e0e8b504866c7deccf1
Ementshh <clementheo.benaya@ui.ac.id>
2026-01-16 09:29:12 +0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Clement's desktop",
   description: "",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
 };
 
 export default function RootLayout({
