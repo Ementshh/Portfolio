a3fc97a698936b719fd9dcfe3da9e7093d23fb72
Ementshh <clementheo.benaya@ui.ac.id>
2026-01-16 08:37:21 +0700

    Contact Clarity

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -5,8 +5,8 @@ import { useAudio } from "../audio/AudioContext";
 
 const contactLinks = [
   { label: "EMAIL", value: "clementheobenaya@gmail.com", href: "mailto:clementheobenaya@gmail.com" },
-  { label: "GITHUB", value: "https://github.com/Ementshh", href: "https://github.com/Ementshh" },
-  { label: "LINKEDIN", value: "www.linkedin.com/in/clementheo-benaya-b521b4315", href: "https://www.linkedin.com/in/clementheo-benaya-b521b4315" },
+  { label: "GITHUB", value: "Ementshh", href: "https://github.com/Ementshh" },
+  { label: "LINKEDIN", value: "Clementheo Benaya", href: "https://www.linkedin.com/in/clementheo-benaya-b521b4315" },
   { label: "LINE", value: "ccllmmnntt", href: "https://line.me/R/ti/p/ccllmmnntt" },
 ];
 
